1304a9765e32cba595cdd532b803f05442e90851
Helix Graziani <helix.graziani@hotmail.com>
2025-01-05 21:29:34 -0500

    Update EASTL to 3.21.23

--- EAWebKitSupportPackages/EASTL/local_3.16.05/scripts/EASTL_strip.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Electronic Arts Inc. All rights reserved.
-
-import SimpleCodeStripper
-
-SimpleCodeStripper.Strip(
-		sourceSubdirectories = ['include', 'source', 'benchmark', 'test'],
-		folderComponentsToExcludeFromCopy=['build', 'scripts', '.git'],
-		filesToCopy=['./scripts/initialize.xml']
-		)
